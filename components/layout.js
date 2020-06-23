9056883c258d1631bc59bdb651f32352593f0d73
TanishqSingla <tanishqsingla00@gmail.com>
2020-06-24 02:28:54 +0530

    Added css to layout

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,7 @@
-function Layout({ component }) {
-  return <div>{component}</div>;
+import styles from "./layout.module.css";
+
+function Layout({ children }) {
+  return <div className={styles.container}>{children}</div>;
 }
 
 export default Layout;
