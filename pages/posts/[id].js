4de8926523ed0b3e5efb2f93624f0d9e02c041ee
TanishqSingla <tanishqsingla00@gmail.com>
2020-06-25 05:13:14 +0530

    loading post ids

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,16 @@
 import Layout from "../../components/layout";
+import { getAllPostIds } from "../../lib/post";
 
 export default function Post() {
   return <Layout></Layout>;
+}
+
+export function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    params: {
+      paths,
+      fallback: false,
+    },
+  };
 }
