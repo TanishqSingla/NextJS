1d004f8434c28474c3e0c2d84b4cdfe8c081e177
TanishqSingla <tanishqsingla00@gmail.com>
2020-06-25 05:51:50 +0530

    polishing posts

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,6 +2,8 @@ import Head from "next/head";
 
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/post";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css";
 
 export default function Post({ postData }) {
   return (
@@ -9,12 +11,13 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
