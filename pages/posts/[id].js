14d1015e396e3f5d34d8e034b1f67d798c35a91f
TanishqSingla <tanishqsingla00@gmail.com>
2020-06-25 05:32:05 +0530

    Fixed typos

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,21 +1,27 @@
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/post";
 
-export default function Post() {
-  return <Layout></Layout>;
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  );
 }
 
-export function getStaticPaths() {
+export async function getStaticPaths() {
   const paths = getAllPostIds();
   return {
-    params: {
-      paths,
-      fallback: false,
-    },
+    paths,
+    fallback: false,
   };
 }
 
-export async function getStaticProps() {
+export async function getStaticProps({ params }) {
   const postData = getPostData(params.id);
   return {
     props: {
